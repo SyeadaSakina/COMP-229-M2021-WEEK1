55d48bf8e59d84bd1dd5ef3365030537ac7f48e4
Syeda Zannatul Sakina Mazid <78048569+SyeadaSakina@users.noreply.github.com>
2021-05-18 12:14:09 -0400

    Add files via upload

---
+++ Week1-C/Scripts/app.js
@@ -0,0 +1,9 @@
+"use strict";
+let myVariable = 20;
+console.log("my number is: " + myVariable);
+
+for(let index=0; index < myVariable; index++) {
+    console.log("your number is " + index);
+}
+
+console.log(index);
